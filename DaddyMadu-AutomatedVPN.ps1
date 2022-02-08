0e93570ecc636edb22b9718d61da21981b714365
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2022-02-09 00:11:23 +0200

    Update DaddyMadu-AutomatedVPN.ps1

--- DaddyMadu-AutomatedVPN.ps1
+++ DaddyMadu-AutomatedVPN.ps1
@@ -1,5 +1,6 @@
 if ((Get-PackageProvider -Name NuGet).version -lt 2.8.5.208 ) {
     try {
+		Write-Host "Checking if Nuget Package is installed..."
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Confirm:$False -Force 
     }
     catch [Exception]{
@@ -14,6 +15,7 @@ if (Get-Module -ListAvailable -Name VPNCredentialsHelper) {
     Write-Host "VPNCredentialsHelper Already Installed"
 }  else {
     try {
+		Write-Host "VPNCredentialsHelper is not found, trying to install it..."
         Install-Module -Name VPNCredentialsHelper -Confirm:$False -Force  
     }
     catch [Exception] {
@@ -23,6 +25,7 @@ if (Get-Module -ListAvailable -Name VPNCredentialsHelper) {
 }
 	Remove-Item -Path "$env:USERPROFILE\VPNServers.txt" -Force -Recurse -ErrorAction SilentlyContinue
 	Remove-Item -Path "$env:USERPROFILE\VPNServersSorted.txt" -Force -Recurse -ErrorAction SilentlyContinue
+	Write-Host "Old VPN Servers list is deleted successfully, Fetching latest avaliable servers..."
 	$VPNServerURI = "https://www.vpngate.net/en/"
     $VPNServerHTML = Invoke-WebRequest -Uri $VPNServerURI
     $VPNServersOut = ($VPNServerHTML.ParsedHtml.getElementsByTagName("td") | Where-Object{$_.className -eq "vg_table_row_1"}).innerText | select-string -CaseSensitive "SSTP Hostname :"
@@ -33,19 +36,23 @@ if (Get-Module -ListAvailable -Name VPNCredentialsHelper) {
     }
 }
 $unsortedservers = Get-Content "$env:USERPROFILE\VPNServers.txt"
+Write-Host "Pinging fetched vpn servers and removing dead ones..."
 @(foreach ($vserver in $unsortedservers){
 test-connection -ComputerName $vserver -Count 1 -ea 0 |
  measure responsetime -average |
  % {$_.average.tostring() + " $vserver"}}) |
  sort |% {$_.split()[1]} | out-file -FilePath "$env:USERPROFILE\VPNServersSorted.txt" -Append
-    get-content "$env:USERPROFILE\VPNServersSorted.txt" | sort{get-random} | select -First 1 | Tee-Object -Variable VPNServerAdress
+    get-content "$env:USERPROFILE\VPNServersSorted.txt" | sort{get-random} | select -First 1 | Tee-Object -Variable VPNServerAdress | Out-Null
     $VPNusername = "vpn"
     $VPNpassword = "vpn"
     $VPNServername = "VPN"
 if ((Get-VpnConnection).name -eq "VPN" ) {
+	Write-Host "VPN Connection found! updating it with the latest fetched vpn server..."
 	Set-VpnConnection -Name VPN -ServerAddress "$VPNServerAdress" -TunnelType "Sstp" -EncryptionLevel "Required" -AuthenticationMethod MSChapv2 -RememberCredential:$true -SplitTunneling:$true -PassThru -ErrorAction SilentlyContinue | Out-Null
 	Set-VpnConnectionUsernamePassword -connectionname $VPNServername -username $VPNusername -password $VPNpassword -ErrorAction SilentlyContinue | Out-Null
 } else { 
+    Write-Host "VPN Connection was not found! adding new one with the latest fetched vpn server..."
     Add-VpnConnection -Name $VPNServername -ServerAddress "$VPNServerAdress" -TunnelType "Sstp" -EncryptionLevel "Required" -AuthenticationMethod MSChapv2 -RememberCredential:$true -SplitTunneling:$true -PassThru -ErrorAction SilentlyContinue | Out-Null
     Set-VpnConnectionUsernamePassword -connectionname $VPNServername -username $VPNusername -password $VPNpassword -ErrorAction SilentlyContinue | Out-Null
 }
+Write-Host "Done!"
